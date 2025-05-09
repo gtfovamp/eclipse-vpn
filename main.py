a1dd646a04aeb081713edb7f7d21b04cf563466b
gtfoghost <59762963+gtfovamp@users.noreply.github.com>
2025-05-09 15:12:41 +0300

    Update main.py

--- main.py
+++ main.py
@@ -17,6 +17,12 @@ app.add_middleware(
     allow_headers=["*"],  # Разрешаем все заголовки
 )
 
+def safe_int(value, default=0):
+    try:
+        return int(value)
+    except (ValueError, TypeError):
+        return default
+
 @app.get("/vpn-data/")
 async def get_vpn_data():
     async with httpx.AsyncClient() as client:
